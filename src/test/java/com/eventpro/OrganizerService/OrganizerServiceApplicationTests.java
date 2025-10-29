8f5f340436dae717a1dbc7450019b4b6021f58c6
RobsonPierno <robson.pierno@gmail.com>
2025-10-29 14:29:05 +0100

    First Commit

---
+++ src/test/java/com/eventpro/OrganizerService/OrganizerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventpro.OrganizerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
