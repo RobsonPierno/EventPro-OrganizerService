50a5c934bc958318008214a368ee1101cb67f294
RobsonPierno <robson.pierno@gmail.com>
2025-10-31 11:49:20 +0100

    Creating module Client with Feign

---
+++ organizer-client/src/main/java/com/eventpro/OrganizerService/dto/OrganizerClient.java
@@ -0,0 +1,30 @@
+package com.eventpro.OrganizerService.dto;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@FeignClient(name = "organizer-service", url = "http://pessoa:8080", path = "/organizer")
+public interface OrganizerClient {
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void create(@RequestBody final OrganizerDTO organizer);
+	
+	@GetMapping("/{id}")
+	public OrganizerDTO findById(@PathVariable final Integer id);
+	
+	@GetMapping
+	public List<OrganizerDTO> findAll(@RequestParam(required = false) final String status);
+	
+	@PatchMapping("/status/{id}")
+	public OrganizerDTO changeStatus(@PathVariable final Integer id);
+}
---
+++ organizer-service/src/main/java/com/eventpro/OrganizerService/controller/OrganizerController.java
@@ -0,0 +1,35 @@
+package com.eventpro.OrganizerService.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eventpro.OrganizerService.dto.OrganizerClient;
+import com.eventpro.OrganizerService.dto.OrganizerDTO;
+import com.eventpro.OrganizerService.service.OrganizerService;
+
+@RestController
+@RequestMapping("/organizer")
+public class OrganizerController implements OrganizerClient {
+	
+	@Autowired
+	private OrganizerService service;
+
+	public void create(final OrganizerDTO organizer) {
+		this.service.create(organizer);
+	}
+	
+	public OrganizerDTO findById(final Integer id) {
+		return this.service.findById(id);
+	}
+	
+	public List<OrganizerDTO> findAll(final String status) {
+		return this.service.findAll(status);
+	}
+	
+	public OrganizerDTO changeStatus(final Integer id) {
+		return this.service.changeStatus(id);
+	}
+}
